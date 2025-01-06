6e0b0601b1605dc44a7bfc98c8b402a183fa9ecc
Skyryo <ryosakaiwmagptja@gmail.com>
2025-01-06 14:22:55 +0900

    性的データの追加とマップ上に路線図の表示v0

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import AppWrapper from "./components/AppWrapper"
 import MapboxComponent from "./components/MapboxComponent"
 import "./App.css"
 
 export default function App() {
   return (
-    <>
+    <AppWrapper>
       <MapboxComponent />
-    </>
+    </AppWrapper>
   )
 }
